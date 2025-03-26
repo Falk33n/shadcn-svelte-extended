f1d2927bf77a32e83aa5dd3680bbc94803c7946f
Falk33n <tim.falk00@gmail.com>
2025-03-26 13:33:25 +0100

    feat: finish input

--- src/lib/components/ui/input/index.ts
+++ src/lib/components/ui/input/index.ts
@@ -1,4 +1,4 @@
-import { default as Input } from './input.svelte';
+import Input from './input.svelte';
 import type { InputProps } from './types';
 
 export { Input, Input as Root, type InputProps, type InputProps as RootProps };
--- src/lib/components/ui/input/types.ts
+++ src/lib/components/ui/input/types.ts
@@ -1,29 +1,34 @@
 import type { WithElementRef, WithoutChildren } from 'bits-ui';
 import type { HTMLInputAttributes, HTMLInputTypeAttribute } from 'svelte/elements';
 
-type InputTypeWithoutFile = Exclude<HTMLInputTypeAttribute, 'file'>;
+//
+// FILE PROPS
+//
 
-type InputPropsWithTypeFile = {
-	/** The input `type` is required to be `"file"` when binding the `files` prop. */
-	type: 'file';
+type ExcludeInputTypeFile = Exclude<HTMLInputTypeAttribute, 'file'>;
 
-	/** Allows binding a `FileList` when the input `type` is `"file"`. */
-	files?: FileList;
-};
+type InputWithoutTypeFileProps = {
+	/** @description Any input `type` other than `"file"`. */
+	type?: ExcludeInputTypeFile;
 
-type InputPropsWithoutTypeFile = {
-	/** Any input `type` other than 'file'. */
-	type?: InputTypeWithoutFile;
-
-	/** The `files` prop is disallowed unless `type` is `"file"`. */
+	/** @description The `files` prop is disallowed unless `type` is `"file"`. */
 	files?: never;
 };
 
-type InputPropsWithoutHTML = WithElementRef<
-	InputPropsWithoutTypeFile | InputPropsWithTypeFile,
-	HTMLInputElement
+type InputWithTypeFileProps = {
+	/** @description The input `type` is required to be `"file"` when binding the `files` prop. */
+	type: 'file';
+
+	/** @description Allows binding a `FileList` when the input `type` is `"file"`. */
+	files?: FileList;
+};
+
+//
+// INPUT PROPS
+//
+
+type InputBaseProps = WithoutChildren<
+	WithElementRef<Omit<HTMLInputAttributes, 'type'>, HTMLInputElement>
 >;
 
-type InputPropsWithHTML = WithoutChildren<Omit<HTMLInputAttributes, 'type'>>;
-
-export type InputProps = InputPropsWithoutHTML & InputPropsWithHTML;
+export type InputProps = InputBaseProps & (InputWithoutTypeFileProps | InputWithTypeFileProps);
