6465f66a1dcefa96b7e2c2ebf67f3b0c86369719
Falk33n <tim.falk00@gmail.com>
2025-03-27 13:55:43 +0100

    feat: add more of bits-ui tooltip components in index

--- src/lib/components/ui/tooltip/index.ts
+++ src/lib/components/ui/tooltip/index.ts
@@ -3,12 +3,21 @@ import TooltipContent, { type TooltipContentProps } from './tooltip-content.svel
 
 const Tooltip = TooltipPrimitive.Root;
 const TooltipTrigger = TooltipPrimitive.Trigger;
+const TooltipArrow = TooltipPrimitive.Arrow;
+const TooltipPortal = TooltipPrimitive.Portal;
+const TooltipProvider = TooltipPrimitive.Provider;
 
 export {
+	TooltipArrow as Arrow,
 	TooltipContent as Content,
+	TooltipPortal as Portal,
+	TooltipProvider as Provider,
 	Tooltip as Root,
 	Tooltip,
+	TooltipArrow,
 	TooltipContent,
+	TooltipPortal,
+	TooltipProvider,
 	TooltipTrigger,
 	TooltipTrigger as Trigger,
 	type TooltipContentProps as ContentProps,
