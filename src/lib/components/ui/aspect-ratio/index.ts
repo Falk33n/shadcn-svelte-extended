4de2d1b41056b6b2b732bee1de871e68a0130f76
Falk33n <tim.falk00@gmail.com>
2025-03-27 16:10:31 +0100

    feat: add aspect ratio

---
+++ src/lib/components/ui/aspect-ratio/index.ts
@@ -0,0 +1,13 @@
+import {
+	AspectRatio as AspectRatioPrimitive,
+	type AspectRatioRootProps as AspectRatioProps,
+} from 'bits-ui';
+
+const AspectRatio = AspectRatioPrimitive.Root;
+
+export {
+	AspectRatio,
+	AspectRatio as Root,
+	type AspectRatioProps,
+	type AspectRatioProps as RootProps,
+};
